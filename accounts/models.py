233c977019077c4d665c8ffa2b44d297e840f02a
Suleiman Alqassem <suliaman3@gmail.com>
2025-11-16 11:04:50 +0300

    Add base, home, login, and signup templates; configure templates directory in settings

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # يمكنك إضافة حقول إضافية لاحقًا مثل phone_number أو avatar
+    # phone_number أو avatar
     pass
