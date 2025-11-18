3516e92fcf835e4e478af400d3c0a9d9db930203
Suleiman Alqassem <suliaman3@gmail.com>
2025-11-18 10:43:14 +0300

    Add Project dropdown widget and label to TaskForm

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,10 +1,15 @@
 from django import forms
-from .models import Task
+from .models import Task,Project
 
 class TaskForm(forms.ModelForm):
+    def __init__(self,*args, **kwargs):
+        user=kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+        self.fields['project'].queryset = Project.objects.filter(owner=user)
+
     class Meta:
         model = Task
-        fields = ['title', 'description', 'status']
+        fields = ['title', 'description', 'status','project']
         widgets = {
             'title': forms.TextInput(attrs={
                 'class': 'form-control',
@@ -18,9 +23,22 @@ class TaskForm(forms.ModelForm):
             'status': forms.Select(attrs={
                 'class': 'form-select'
             }),
+            'project': forms.Select(attrs={
+                'class': 'form-select'
+                }),
         }
         labels = {
             'title': 'Title',
             'description': 'Description',
             'status': 'Status',
+            'project':'Project',
+        }
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
